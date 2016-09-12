2b551b644da96da00d90f83a8145b8529419e855
cloworm <chloe.echikson@gmail.com>
2016-09-12 12:43:24 -0400

    Created basic model for orders

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,9 @@ module.exports = db;
 
 // eslint-disable-next-line no-unused-vars
 var User = require('./models/user');
+// eslint-disable-next-line no-unused-vars
+var Order = require('./models/order');
 
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
+User.hasMany(Order);
