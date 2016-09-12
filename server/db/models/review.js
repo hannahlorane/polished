8c7106d2805be0f48a31419437e4b6bcddb07ec1
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-09-12 12:56:42 -0400

    added validation for length of review text

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -2,7 +2,12 @@ var db = require('../_db');
 var Sequelize = require('sequelize');
 
 module.exports = db.define('review', {
-  text: Sequelize.TEXT,
+  text: {
+    type: Sequelize.TEXT,
+    validate: {
+      len: [200, 10000]
+    }
+  },
   stars: {
     type: Sequelize.INTEGER,
     allowNull: false,
