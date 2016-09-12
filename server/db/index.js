9930c2fbc316434512e1c27345f5c5e2857027cf
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-09-12 12:43:35 -0400

    created review model

--- server/db/index.js
+++ server/db/index.js
@@ -7,3 +7,5 @@ var User = require('./models/user');
 
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
+User.hasMany(Review);
+Product.hasMany(Review);
---
+++ server/db/models/review.js
@@ -0,0 +1,11 @@
+var db = require('./_db');
+var Sequelize = require('sequelize');
+
+module.exports = db.define('review', {
+  text: Sequelize.TEXT,
+  stars: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    range: [0, 5]
+  }
+});
