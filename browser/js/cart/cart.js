1a1b7673d1fa76509bbef36da12cc566f15e2def
cloworm <chloe.echikson@gmail.com>
2016-09-13 18:26:42 -0400

    have basic cart layout

---
+++ browser/js/cart/cart.js
@@ -0,0 +1,13 @@
+app.config(function ($stateProvider) {
+
+  $stateProvider.state('cart', {
+    url: '/cart',
+    controller: 'CartController',
+    templateUrl: 'js/cart/cart.html'
+  });
+
+});
+
+app.controller('CartController', function ($scope) {
+
+});
