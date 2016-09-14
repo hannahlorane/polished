730eb79bbf57e779eab3fef94e4b8ab74d0e4d46
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-09-14 13:00:35 -0400

    main skeleton of single-product view, except for the similar products

--- browser/js/product/product.controller.js
+++ browser/js/product/product.controller.js
@@ -1,7 +1,12 @@
-app.controller('productController', function (productFactory, $scope, product, $stateParams) {
+app.controller('productController', function (CartFactory, productFactory, $scope, product, $stateParams) {
   $scope.product = product;
+  console.log(product);
   productFactory.getReviews(product.id)
     .then(function (reviews) {
       $scope.product.reviews = reviews;
-    })
+    });
+  $scope.addToCart = function () {
+    console.log("added " + product.name + " to cart");
+  }
+  //= CartFactory.addToCart(product.id, SESSION ID);
 });
