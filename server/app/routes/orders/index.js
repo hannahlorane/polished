bf10cf8d1963cc7a9b0878685018bfa4020efc32
cloworm <chloe.echikson@gmail.com>
2016-09-14 12:03:02 -0400

    added CRUD routes for OrderProducts

--- server/app/routes/orders/index.js
+++ server/app/routes/orders/index.js
@@ -105,16 +105,78 @@ router.delete('/:id', function (req, res) {
     });
 });
 
+// Get all the products for an order
 router.get('/:id/products', function (req, res, next) {
   OrderProducts.findAll({
     where: {
       orderId: req.params.id
     }
   })
+  .then(function (products) {
+    res.send(products);
+  })
+  .catch(next);
+})
+
+// Add a product to the order with body {productId: productId, qty: qty}
+router.post('/:id/products', function (req, res, next) {
+  req.body.orderId = req.params.id;
+
+  OrderProducts.create(req.body)
+  .then(function(newItem) {
+    res.send(newItem);
+  })
+  .catch(next);
+})
+
+// Get a specific product from an order
+router.get('/:id/products/:productId', function (req, res, next) {
+  OrderProducts.findOne({
+    where: {
+      orderId: req.params.id,
+      productId: req.params.productId
+    }
+  })
   .then(function (product) {
     res.send(product);
   })
   .catch(next);
+})
+
+// Update the quantity for an order
+router.put('/:id/products/:productId', function (req, res, next) {
+  OrderProducts.findOne({
+    where: {
+      orderId: req.params.id,
+      productId: req.params.productId
+    }
+  })
+  .then(function(orderProduct) {
+    return orderProduct.update(req.body);
+  })
+  .then(function(orderProductUpdated) {
+    res.send(orderProductUpdated);
+  })
+  .catch(next);
+})
+
+// Delete a product from an order
+router.delete('/:id/products/:productId', function (req, res, next) {
+  OrderProducts.findOne({
+    where: {
+      orderId: req.params.id,
+      productId: req.params.productId
+    }
+  })
+  .then(function (orderProduct) {
+    return orderProduct.destroy()
+  })
+  .then(function (result) {
+    res.send(result);
+  })
+  .catch(function () {
+    res.sendStatus(500);
+  });
 })
 
 module.exports = router;
