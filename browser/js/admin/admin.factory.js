7a7cfcd007de995cad90b38fa7d90e418148845c
cloworm <chloe.echikson@gmail.com>
2016-09-14 23:07:40 -0400

    admin can edit individual products

--- browser/js/admin/admin.factory.js
+++ browser/js/admin/admin.factory.js
@@ -10,5 +10,14 @@ app.factory('AdminFactory', function ($http, $state) {
     })
   }
 
+  methods.editProduct = function(id, body) {
+    body.rgbValue = [body.rgbValue['0'], body.rgbValue['1'], body.rgbValue['2']];
+
+    return $http.put('/api/products/' + id, body)
+    .then(function(response) {
+      return response.data;
+    })
+  }
+
   return methods;
 })
--- browser/js/admin/products/edit/products.edit.state.js
+++ browser/js/admin/products/edit/products.edit.state.js
@@ -1,9 +1,14 @@
 app.config(function ($stateProvider) {
 
   $stateProvider.state('admineditproducts', {
-    url: '/admin/products/edit',
+    url: '/admin/products/edit/:id',
     templateUrl: 'js/admin/products/edit/editproducts.html',
-    controller: 'ProductEditController'
+    controller: 'ProductEditController',
+    resolve: {
+      product: function(productFactory, $stateParams) {
+        return productFactory.getProductById($stateParams.id);
+      }
+    }
   });
 
 });
