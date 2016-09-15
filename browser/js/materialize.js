fa1ad581d33cf8a5d5f878fd1dca9809d4e1ff9e
emilymalecbrown <emilymalecbrown@gmail.com>
2016-09-14 21:01:50 -0400

    home page is sufficient for now

--- browser/js/materialize.js
+++ browser/js/materialize.js
@@ -4,4 +4,6 @@ $(document).ready(function() {
   });
   $(".dropdown-button").dropdown();
   $('select').material_select();
+  $('.parallax').parallax();
+  $('.slider').slider({full_width: true, height: 350});
 })
