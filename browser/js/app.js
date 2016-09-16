f51ebd5045123ab33a531f656cb0deee6920dfca
cloworm <chloe.echikson@gmail.com>
2016-09-16 12:53:37 -0400

    signups post to the db

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-window.app = angular.module('polish', ['fsaPreBuilt', 'ui.router', 'ngAnimate']);
+window.app = angular.module('polish', ['fsaPreBuilt', 'ui.router', 'ngAnimate', 'ui.validate']);
 
 app.config(function ($urlRouterProvider, $locationProvider) {
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
