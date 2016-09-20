d113fd307a63798e56e465aec6c0ba40fbc1a504
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-09-20 11:36:33 -0400

    kleene star not displayed in the search bar

--- browser/js/catalog/catalog.controller.js
+++ browser/js/catalog/catalog.controller.js
@@ -15,10 +15,10 @@ app.controller('catalogController', function ($scope, $state, productFactory, pr
     Blue: [0, 0, 255],
     Purple: [211, 85, 165],
     Pink: [255, 75, 110],
-    Light: [255, 255, 255],
+    Light: [275, 275, 275],
     Dark: [0, 0, 0]
   };
-  $scope.searched = '.*';
+  $scope.searchDefault = '.*';
   $scope.collectionView = false;
   $scope.colorNames = Object.keys($scope.allColors);
 
